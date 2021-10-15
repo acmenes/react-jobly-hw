9ffeaa2914282d4498ebd34a90e9daf1d2aad311
acmenes <acmenescompositions@gmail.com>
2021-10-15 08:53:09 -0400

    added basic functionality for signup and login

--- src/App.js
+++ src/App.js
@@ -8,13 +8,41 @@ import HomePage from "./homepage/HomePage";
 import CompanyList from "./companies/CompanyList";
 import Jobs from "./jobs/Jobs";
 import Profile from "./profiles/Profile";
-import Signup from "./auth/Signup";
-import Login from "./auth/Login";
+import SignupForm from "./auth/Signup";
+import LoginForm from "./auth/Login";
+import UserContext from "./auth/UserContext";
 
 import './App.css';
 
 
-function App() {
+function App({ login, signup }) {
+  
+  /** Authenticate and sign up a user  */
+  async function signup(signupData) {
+    // try {
+    //   let token = await JoblyApi.signup(signupData);
+    //   setToken(token);
+    //   return { success: true };
+    // } catch (errors) {
+    //   console.error("signup failed", errors);
+    //   return { success: false, errors };
+    // }
+    alert("you have been signed up")
+  }
+
+  /** authenticate and log in a user */
+  async function login(loginData) {
+    // try {
+    //   let token = await JoblyApi.login(loginData);
+    //   setToken(token);
+    //   return { success: true };
+    // } catch (errors) {
+    //   console.error("login failed", errors);
+    //   return { success: false, errors };
+    // }
+    alert("logged in")
+  }
+  
   return (
     <div className="App">
         <BrowserRouter>
@@ -25,10 +53,10 @@ function App() {
                 <HomePage />
               </Route>
               <Route exact path="/login">
-                <Login />
+                <LoginForm login={login} />
               </Route>
               <Route exact path="/signup">
-                <Signup />
+                <SignupForm signup={signup} />
               </Route>
               <Route exact path="/jobs">
                 <Jobs />
